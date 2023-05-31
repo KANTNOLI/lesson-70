3f9d727cbacf404c8617a8c3a457c74120c7cfe8
KANTNOLI <zahargysev02@gmail.com>
2023-05-31 17:23:18 +0300

    123

---
+++ lesson 70/Figure.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "main.h"
+
+class Figure
+{
+
+public:
+	double calculatePerimeter();
+	double calculateSquare();
+	string description();
+};
+
---
+++ lesson 70/main.cpp
@@ -0,0 +1,7 @@
+#include "FigureEditor.h"
+
+int main() {
+
+
+	return 0;
+}
