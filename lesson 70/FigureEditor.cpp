6a7007b7c5db3d666c6a2388841c989232084692
KANTNOLI <zahargysev02@gmail.com>
2023-05-31 17:37:13 +0300

    ar

--- lesson 70/FigureEditor.cpp
+++ lesson 70/FigureEditor.cpp
@@ -3,6 +3,6 @@
 string FigureEditor::get_info(Figure* figure) {
 	string msg = figure->description();
 	msg += "\nPerimeter = " + to_string(figure->calculatePerimeter());
-	msg += "\Square = " + to_string(figure->calculateSquare());
+	msg += "\nSquare = " + to_string(figure->calculateSquare());
 	return msg;
 }
--- lesson 70/main.cpp
+++ lesson 70/main.cpp
@@ -1,7 +1,12 @@
 #include "FigureEditor.h"
+#include "triangle.h"
 
-int main() {
+int main(void) {
+	Figure* f1 = new Figure();
 
+
+
+	cout << FigureEditor::get_info(f1) << endl;
 
 	return 0;
 }
